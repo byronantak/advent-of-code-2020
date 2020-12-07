286546e80163ded7ca37a03199a184d8a3bdd90b
Byron Antak <bma.byron.antak@gmail.com>
2020-12-07 21:57:30 +0200

    keep files consistent with tabs. remove console logs

--- day2/main_part2.js
+++ day2/main_part2.js
@@ -3,29 +3,29 @@ const fs = require('fs')
 console.log(getValidNumberPasswords('./puzzle_input.txt'))
 
 function getValidNumberPasswords(filename) {
-	try {
-		return fs.readFileSync(filename, 'utf8')
-			.split('\n')
-			.reduce((total, line) => {
-				if (isLineValid(line)) {
-					console.log(line);
-					return total + 1;
-				}
-				return total;
-			}, 0);
-	} catch (err) {
-		console.error(err)
-	}
+    try {
+        return fs.readFileSync(filename, 'utf8')
+            .split('\n')
+            .reduce((total, line) => {
+                if (isLineValid(line)) {
+                    console.log(line);
+                    return total + 1;
+                }
+                return total;
+            }, 0);
+    } catch (err) {
+        console.error(err)
+    }
 }
 
 function isLineValid(line) {
-	return isPasswordValidLong(
-		line.split(': ')[0].split(' ')[1], 
-		line.split(': ')[0].split(' ')[0].split('-')[0],
-	 	line.split(': ')[0].split(' ')[0].split('-')[1],
-	 	line.split(': ')[1]);
+    return isPasswordValidLong(
+        line.split(': ')[0].split(' ')[1], 
+        line.split(': ')[0].split(' ')[0].split('-')[0],
+        line.split(': ')[0].split(' ')[0].split('-')[1],
+        line.split(': ')[1]);
 }
 
 function isPasswordValidLong(char, pos1, pos2, password) {
-	return password[pos1 - 1] == char ^ password[pos2 - 1] == char;
+    return password[pos1 - 1] == char ^ password[pos2 - 1] == char;
 }
--- day6/main.js
+++ day6/main.js
@@ -1,28 +1,28 @@
 const fs = require('fs')
 
 function distinctFilter(value, index, self) {
-	return self.indexOf(value) === index;
+    return self.indexOf(value) === index;
 }
 
 function distinctCharacterReduce(total, curr) {
-	if (curr && curr.length && curr.length > 1) {
-		return curr.split('').reduce(distinctCharacterReduce, total);
-	}
+    if (curr && curr.length && curr.length > 1) {
+        return curr.split('').reduce(distinctCharacterReduce, total);
+    }
 
-	if (total.indexOf(curr) == -1) {
-		return total + curr;
-	}
-	return total;
+    if (total.indexOf(curr) == -1) {
+        return total + curr;
+    }
+    return total;
 }
 
 function process(string) {
-	return string.split('\r\n\r\n')
-				.map(x => {
-					return x.split('\r\n')
-							.filter(distinctFilter)
-							.reduce(distinctCharacterReduce, '')
-				})
-				.length;
+    return string.split('\r\n\r\n')
+                .map(x => {
+                    return x.split('\r\n')
+                            .filter(distinctFilter)
+                            .reduce(distinctCharacterReduce, '')
+                })
+                .length;
 }
 
 console.log(process(fs.readFileSync('puzzle_input.txt', 'utf8')));
